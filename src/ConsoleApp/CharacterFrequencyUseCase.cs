2461f7d01e16528ac5089408e2064e6333275e60
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-11-07 07:30:53 -0300

    feat: solve challenge

---
+++ src/ConsoleApp/CharacterFrequencyUseCase.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+internal static class CharacterFrequencyUseCase
+{
+    internal static int Execute(string filePath, char searchByCharacter)
+    {
+        const int bufferSize = 1024;
+
+        byte[] buffer = new byte[bufferSize];
+
+        int bytesRead;
+
+        int characterFrequency = 0;
+
+        using FileStream stream = new(filePath, FileMode.Open, FileAccess.Read);
+
+        while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            string content = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+
+            foreach (char character in content)
+            {
+                if (character == searchByCharacter)
+                {
+                    characterFrequency++;
+                }
+            }
+        }
+
+        return characterFrequency;
+    }
+}
--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+Stopwatch stopwatch = new();
+
+stopwatch.Start();
+
+const string filePath = "./largeFile.txt";
+
+const char character = 'A';
+
+int frequency = CharacterFrequencyUseCase.Execute(filePath, character);
+
+stopwatch.Stop();
+
+Console.WriteLine($"Total de vezes que se registrou o caractere {character}: {frequency}");
+
+Console.WriteLine($"Arquivo processado em: {stopwatch.ElapsedMilliseconds} ms");
